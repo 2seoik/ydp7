ef452069198c2d0be56f5c1358db7c2e713fc223
seoik <seoikk21@gmail.com>
2025-06-27 12:38:19 +0000

    250627 update

--- javascript/array/practice1.js
+++ javascript/array/practice1.js
@@ -1,29 +1,41 @@
 const assert = require("assert");
 
+const arr = [1, 2, 3, 4];
 /**
  * 다음과 같은 push, pop, shift, unshift 를 순수 함수로 작성하시오.
  * (단, 입력값은 다음 예시로 한정함)
  */
 
 const push = (...args) => {
-  const [array, ...params] = [...args];
-  return array.concat(params);
+    const [array, ...params] = [...args];
+    return array.concat(params);
 };
+
+// 다른방법
+// const push = (arr, ...args) => {
+//     return [...arr, ...args]
+// }
 
 // slice?
 // 마지막 값 pop
+// array.at
 const pop = (array, range) => (range ? array.slice(range) : +array.slice(-1));
 
 //
 const unshift = (...args) => {
-  const [array, ...params] = [...args];
-  return params.concat(array);
+    const [array, ...params] = [...args];
+    return params.concat(array);
 };
+
+// 다른방법
+// const unshift2 = (arr, ...args) => {
+//     return [...args, ...arr]
+// }
 
 //
 const shift = (array, range = 1) => [array.slice(0, range), array.slice(range)];
 
-const arr = [1, 2, 3, 4];
+
 assert.deepStrictEqual(push(arr, 5, 6), [1, 2, 3, 4, 5, 6]);
 assert.deepStrictEqual(pop(arr), 4);
 assert.deepStrictEqual(pop(arr, 2), [3, 4]); // 2개 팝!
@@ -31,7 +43,7 @@ assert.deepStrictEqual(unshift(arr, 0), [0, 1, 2, 3, 4]);
 assert.deepStrictEqual(unshift(arr, 7, 8), [7, 8, 1, 2, 3, 4]);
 assert.deepStrictEqual(shift(arr), [[1], [2, 3, 4]]); // [shift되는 원소들, 남은 원소들]
 assert.deepStrictEqual(shift(arr, 2), [
-  [1, 2],
-  [3, 4],
+    [1, 2],
+    [3, 4],
 ]); // 2개 shift
 assert.deepStrictEqual(arr, [1, 2, 3, 4]);
--- javascript/array/practice3.js
+++ javascript/array/practice3.js
@@ -22,11 +22,17 @@ users.changeUser = (p1, p2) => {
 // users 객체에 함수를 추가하여 사용하는경우, users 객체가 오염되어
 // assert.deepStrictEqual(users, [kim, lee, park]) 통과 불가함.
 // 어떻게 해야하는지 여쭤볼것...
-// console.log(users);
+// 아래 참조 ==>
+
+const fnName = Object.keys(users).filter(isNaN);
+
+fnName.map((x) => {
+  Object.defineProperty(users, x, { enumerable: false })
+})
 
 assert.deepStrictEqual(users.addUser(hong), [kim, lee, park, hong]);
-// assert.deepStrictEqual(users, [kim, lee, park]);
+assert.deepStrictEqual(users, [kim, lee, park]);
 assert.deepStrictEqual(users.removeUser(lee), [kim, park]);
-// assert.deepStrictEqual(users, [kim, lee, park]);
+assert.deepStrictEqual(users, [kim, lee, park]);
 assert.deepStrictEqual(users.changeUser(kim, choi), [choi, lee, park]);
-// assert.deepStrictEqual(users, [kim, lee, park]);
+assert.deepStrictEqual(users, [kim, lee, park]);
