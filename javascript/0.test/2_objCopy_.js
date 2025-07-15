0e62d463ad87924f507107c0cb08994b2d8055c2
seoik <seoikk21@gmail.com>
2025-07-15 16:15:44 +0000

    javascript 테스트 준비

---
+++ javascript/0.test/2_objCopy_.js
@@ -0,0 +1,37 @@
+const assert = require("assert")
+
+/*
+원시값(primitive)만을 갖는 객체 kim을 복사하는 프로그램을 Object의 클래스 메소드 또는 spread(...) 연산자를  사용하지 말고 작성하시오.
+
+// 1) shallow copy
+const kim = {nid: 3, nm: 'Kim', addr: 'Pusan'};
+const newKim1 = shallowCopy(kim);
+newKim1.addr = 'Daegu';
+console.log(kim.addr !== newKim1.addr); // true면 통과!
+// 2) 이하 deep copy
+const kim2 = {nid: 3, nm: 'Kim', addr: {city: 'Pusan', road: 'Haeundaero', zip: null }};
+const newKim2 = deepCopy(kim2); 
+newKim2.addr.city = 'Daegu';
+console.log(kim2.addr.city !== newKim2.addr.city); // true면 통과!
+*/
+
+const shallowCopy = (object) => {
+
+}
+
+
+const deepCopy = (object) => {
+
+}
+
+const kim = { nid: 3, nm: 'Kim', addr: 'Pusan' };
+const newKim1 = shallowCopy(kim);
+newKim1.addr = 'Daegu';
+console.log(kim.addr !== newKim1.addr);
+
+
+const kim2 = { nid: 3, nm: 'Kim', addr: { city: 'Pusan', road: 'Haeundaero', zip: null } };
+const newKim2 = deepCopy(kim2);
+newKim2.addr.city = 'Daegu';
+console.log(kim2.addr.city !== newKim2.addr.city);
+
